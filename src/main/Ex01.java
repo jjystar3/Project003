758e03792c923b1c33beb4006834a6cdcfa1d58d
Ju Young <jjystar3@gmail.com>
2024-06-24 16:49:57 +0900

    프로젝트3 생성

---
+++ src/main/Ex01.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = -20;
+		
+		System.out.println(+num1);
+		System.out.println(+num2);
+
+		System.out.println(-num1);
+		System.out.println(-num2);
+		
+		System.out.println(num1);
+		System.out.println(num2);
+	}
+
+}
---
+++ src/quiz/Quiz01.java
@@ -0,0 +1,18 @@
+package quiz;
+
+public class Quiz01 {
+
+	public static void main(String[] args) {
+		
+		int num = -10;
+		int result = +num; // -10
+		
+		int num2 = -20;
+		int result2 = -num2; // 20
+		
+		System.out.println(result);
+		System.out.println(result2);
+		
+	}
+
+}
